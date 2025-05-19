c1422e9305681bbad6902149356f362364bc3f2a
Ayush Kamat <ayush@latch.bio>
2025-05-19 12:21:32 -0700

    add support for literals and unions
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- latch_config/config.py
+++ latch_config/config.py
@@ -1,7 +1,26 @@
 import os
-from dataclasses import MISSING, dataclass, field, fields, is_dataclass
+from dataclasses import (
+    MISSING,
+    Field,
+    dataclass,
+    field,
+    fields,
+    is_dataclass,
+)
 from enum import Enum
-from typing import TypeVar
+from textwrap import indent
+from types import UnionType
+from typing import (
+    Any,
+    ClassVar,
+    Literal,
+    Protocol,
+    Self,
+    TypeVar,
+    Union,
+    get_args,
+    get_origin,
+)
 
 
 @dataclass(frozen=True)
@@ -51,7 +70,76 @@ class LoggingMode(str, Enum):
 T = TypeVar("T")
 
 
-def read_config(x: type[T], env_prefix: str = "") -> T:
+class _IsDataclass(Protocol):
+    __dataclass_fields__: ClassVar[dict[str, Field]]
+
+
+DC = TypeVar("DC", bound=_IsDataclass)
+
+
+class NestyError(Exception):
+    def __init__(self, msg: str, children: list[Self] | None = None):
+        self.msg = msg
+        self.children = children
+
+    def __str__(self):
+        if self.children is None:
+            return self.msg
+
+        res: list[str] = [self.msg]
+        for child in self.children:
+            res.extend(indent(str(child), " -> "))
+
+        return "\n".join(res)
+
+
+def parse_config_val(t: type[T], v: Any) -> T:
+    if t is type(None):
+        if v is None:
+            return v
+
+        raise NestyError(f"value `{repr(v)}` is not assignable to `type(None)`")
+
+    origin, args = get_origin(t), get_args(t)
+
+    if origin is None:
+        try:
+            return t(v)
+        except Exception as e:
+            raise NestyError(
+                f"value `{repr(v)}` is not assignable to type `{t}`: {e}"
+            ) from e
+
+    if origin in {Union, UnionType}:
+        errors: list[NestyError] = []
+        for arg in args:
+            try:
+                return parse_config_val(arg, v)
+            except NestyError as e:
+                errors.append(e)
+
+        raise NestyError(
+            f"value `{repr(v)}` is not assignable to union type `{t}`", errors
+        )
+
+    if origin is Literal:
+        errors: list[NestyError] = []
+        for arg in args:
+            if v == arg:
+                return v
+            else:
+                errors.append(
+                    NestyError(f"value `{repr(v)}` is not equal to literal `{arg}`")
+                )
+
+        raise NestyError(
+            f"value `{repr(v)}` is not assignable to literal type `{t}`", errors
+        )
+
+    raise NotImplementedError(f"config parsing is not implemented for type {t}")
+
+
+def read_config(x: type[DC], env_prefix: str = "") -> DC:
     res = {}
     for f in fields(x):
         val = None
@@ -71,7 +159,7 @@ def read_config(x: type[T], env_prefix: str = "") -> T:
                 if parser is not None:
                     val = parser(env_val)
                 else:
-                    val = typ(env_val)
+                    val = parse_config_val(typ, env_val)
 
         if val is None:
             if f.default != MISSING:
